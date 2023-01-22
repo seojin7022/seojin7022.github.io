e2340c44f63e9ec8acd0c43819d05288f09e8df0
seojin7022 <seojin7022@gmail.com>
2023-01-22 12:53:17 +0900

    Image Load Speed

--- JS/background.js
+++ JS/background.js
@@ -1,18 +1,12 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 function getRandom(maxNum) {
-    return Math.floor(Math.random() * maxNum);
+  return Math.floor(Math.random() * maxNum);
 }
 
 const chosenImage = images[getRandom(images.length)];
 const bgImage = document.createElement("img");
 
-
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `https://cdn.jsdelivr.net/gh/seojin7022/seojin7022.github.io/img/${chosenImage}`;
 document.body.appendChild(bgImage);
 bgImage.classList.add("bgImage");
