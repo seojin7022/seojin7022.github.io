b677cd8051b1cfbe4dc6f2f861fc576442fd71be
JINI7022 <pgptv7022@gmail.com>
2023-01-23 20:42:05 +0900

    Commit

--- JS/search.js
+++ JS/search.js
@@ -1,10 +1,29 @@
 const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-input");
+
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+}
+
 const handleSubmit = (event) => {
   event.preventDefault();
-  window.location.replace(
-    `https://www.google.com/search?q=${event.target[0].value}`
-  );
+  if (validURL(event.target[0].value)) 
+  {
+    window.location.replace(event.target[0].value);
+  }
+
+  else {
+    window.location.replace(
+      `https://www.google.com/search?q=${event.target[0].value}`
+    );
+  }
+  
 };
 
 const handleFocus = (event) => {
